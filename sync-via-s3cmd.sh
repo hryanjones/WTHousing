ea14df6a0181993b98100d00f6917b8368782a32
H. Ryan Jones <hryanjones@gmail.com>
2015-02-24 02:24:18 -0800

    tweaking sync commands to be more thorough and fix that stupid CSS file

--- sync-via-s3cmd.sh
+++ sync-via-s3cmd.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-s3cmd --exclude=".git*" --no-check-md5 sync ~/Documents/github/WTHousing/ s3://hryanjones.com/tmp/WTHousing/
+s3cmd --exclude=".git*" sync ~/Documents/github/WTHousing/ s3://hryanjones.com/tmp/WTHousing/
+s3cmd -m "text/css" put assets/styles/theme.css  s3://hryanjones.com/tmp/WTHousing/assets/styles/theme.css  # should probably figure out why it's setting the mime-type wrong :(
